80c1d34cb53428a3e76cf5690968570eff94deeb
SRobles97 <s.robles01@ufromail.cl>
2023-11-25 01:05:51 -0300

    Añadido valor del dado y mostrado valor en pantalla

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -15,13 +15,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String _diceImage = 'dice-2.png';
   bool _isRolling = false;
+  int _diceNumber = 2;
+  bool _showResult = false;
 
   void _rollDice() {
     final random = Random();
     final randomNumber = random.nextInt(6) + 1;
     setState(() {
       _diceImage = 'dice-$randomNumber.png';
+      _diceNumber = randomNumber;
       _isRolling = true;
+      _showResult = true;
     });
     Future.delayed(const Duration(milliseconds: 500), () {
       setState(() {
@@ -52,7 +56,17 @@ class _HomePageState extends State<HomePage> {
             onPressed: _rollDice,
             color: const Color(0xFFF0F6F6),
             borderColor: const Color(0xFFF0F6F6),
-          )
+          ),
+          if (_showResult) const SizedBox(height: 20),
+          if (_showResult)
+            Text(
+              'You rolled a $_diceNumber',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
         ],
       ),
     ));
